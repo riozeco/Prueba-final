37253782c826d1fc71a7dd7754b49217c70f6f42
Scarlet Rioseco <sriozeco@gmail.com>
2021-05-29 22:46:39 -0400

    Botones y JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 
+$(function(){
+    $("#enviarCorreo").click(function(){
+        alert("El correo fue enviado correctamente...");
+    });
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    $(".card-title").click(function(){
+      $(".card-text").toggle();
+    })
+});
